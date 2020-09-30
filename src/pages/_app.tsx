429af33e76671665be4bc7df6f61fdd69a454942
Kristóf Poduszló <kripod@protonmail.com>
2020-09-30 23:38:34 +0200

    refactor: type-check source files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
 
