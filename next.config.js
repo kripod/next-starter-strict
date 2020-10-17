cea7f3ce7ed651afc703aef8dd1b00471c841730
Kristóf Poduszló <kripod@protonmail.com>
2020-10-17 20:03:18 +0200

    feat: add module analyzer script

---
+++ next.config.js
@@ -0,0 +1,6 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+const withPlugins = require("next-compose-plugins");
+
+module.exports = withPlugins([
+	withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" }),
+]);
