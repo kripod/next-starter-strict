b4815a0788ec29945a2e03663ca02df651a160c9
Kristóf Poduszló <kripod@protonmail.com>
2020-10-17 16:49:39 +0200

    chore: set up Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+		defaultLineHeights: true,
+		standardFontWeights: true,
+	},
+	purge: ["./src/**/*.{js,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
