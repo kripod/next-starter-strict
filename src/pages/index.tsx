c4501af3efd388252d48dbbfd33a2fc15f45cde8
Kristóf Poduszló <kripod@protonmail.com>
2020-10-17 19:24:05 +0200

    docs: improve example page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import clsx from "clsx";
+import { useState } from "react";
+
 export default function Page(): React.ReactElement | null {
+	const [inverted, setInverted] = useState(false);
+
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen">
-			Hello, world!
+		<div
+			className={clsx(
+				"flex flex-col items-center justify-center min-h-screen",
+				inverted && "bg-black",
+			)}
+		>
+			<button
+				type="button"
+				className={clsx(
+					"p-3 border-4 border-current",
+					inverted && "text-white",
+				)}
+				onClick={() => {
+					setInverted((prevValue) => !prevValue);
+				}}
+			>
+				Invert colors
+			</button>
 		</div>
 	);
 }
