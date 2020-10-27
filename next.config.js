184b8ca12c42421d8676ced50b499eddb59f162d
Kristóf Poduszló <kripod@protonmail.com>
2020-10-27 22:57:41 +0100

    feat: enable React's strict mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([
-	withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" }),
-]);
+module.exports = withPlugins(
+	[withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })],
+	{
+		reactStrictMode: true,
+	},
+);
