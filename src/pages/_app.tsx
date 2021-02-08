76316d66b9bfda31b0cf40a7f90694f38956bb44
Kristóf Poduszló <kripod@protonmail.com>
2021-02-08 01:05:57 +0100

    feat: color-scheme preference placeholder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,19 @@ import "@/styles.css";
 import "@/polyfills";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta
+					name="color-scheme"
+					content="normal" // TODO: Set to "dark" or "light" based on theme
+				/>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
