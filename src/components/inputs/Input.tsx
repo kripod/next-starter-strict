ab7f3e3a5171f8e6fea8552d51f33f8d703f16c2
Kristóf Poduszló <kripod@protonmail.com>
2021-02-13 12:16:40 +0100

    feat(Input): experiment with placeholder colors

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -6,7 +6,7 @@ export function Input({ className, ...restProps }: InputProps) {
 	return (
 		<input
 			className={clsx(
-				"px-3 h-11 bg-transparent border focus:border-blue-500 border-gray-500 rounded focus:outline-none appearance-none ring-gray-300 dark:ring-gray-700 dark:ring-offset-black ring-offset-1 focus:ring",
+				"placeholder-gray-700 dark:placeholder-gray-300 placeholder-opacity-60 dark:placeholder-opacity-50 px-3 h-11 bg-transparent border focus:border-blue-500 border-gray-500 rounded focus:outline-none appearance-none ring-gray-300 dark:ring-gray-700 dark:ring-offset-black ring-offset-1 focus:ring",
 				className,
 			)}
 			{...restProps}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
 	},
 	variants: {
 		extend: {
+			placeholderOpacity: ["dark"],
 			ringWidth: ["focus-visible"],
 			transitionProperty: ["motion-reduce"],
 		},
