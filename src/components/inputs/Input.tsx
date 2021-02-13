5b0f6ecb4b60dd107c357f974024b2ca1d3089cf
Kristóf Poduszló <kripod@protonmail.com>
2021-02-13 11:21:02 +0100

    feat(Input): recolor border on focus

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,5 +1,5 @@
 export function Input() {
 	return (
-		<input className="px-3 h-11 bg-transparent border border-gray-500 rounded focus:outline-none appearance-none ring-gray-300 dark:ring-gray-700 dark:ring-offset-black ring-offset-1 focus:ring" />
+		<input className="px-3 h-11 bg-transparent border focus:border-blue-500 border-gray-500 rounded focus:outline-none appearance-none ring-gray-300 dark:ring-gray-700 dark:ring-offset-black ring-offset-1 focus:ring" />
 	);
 }
