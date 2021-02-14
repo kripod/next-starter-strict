2b0cba174f4d4c3397b04d5e4bd35d694e8ee7d9
Kristóf Poduszló <kripod@protonmail.com>
2021-02-14 20:00:43 +0100

    refactor(Input): improve colors

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -10,17 +10,17 @@ export function Input({ invalid, className, style, ...restProps }: InputProps) {
 		<span className={clsx("inline-grid items-center", className)} style={style}>
 			<input
 				className={clsx(
-					"placeholder-gray-700 dark:placeholder-gray-300 placeholder-opacity-75 dark:placeholder-opacity-60 col-start-1 row-start-1 px-3 h-11 bg-transparent rounded focus:outline-none appearance-none dark:ring-offset-black ring-offset-1 ring-opacity-50 focus:ring",
+					"placeholder-gray-700 dark:placeholder-gray-300 placeholder-opacity-75 dark:placeholder-opacity-60 col-start-1 row-start-1 px-3 h-11 bg-transparent rounded focus:outline-none appearance-none dark:ring-offset-black ring-offset-1 ring-opacity-50 dark:ring-opacity-50 focus:ring",
 					invalid
-						? "pr-11 border-2 border-red-500 ring-red-500"
-						: "border border-gray-500 focus:border-blue-500 ring-blue-500",
+						? "pr-11 border-2 border-red-600 dark:border-red-400 ring-red-600 dark:ring-red-400"
+						: "border border-gray-500 focus:border-blue-600 dark:focus:border-blue-400 ring-blue-600 dark:ring-blue-400",
 				)}
 				{...restProps}
 			/>
 			{invalid && (
 				<ExclamationCircle
 					aria-hidden
-					className="col-start-1 row-start-1 justify-self-end px-3 h-6 text-red-500 pointer-events-none"
+					className="col-start-1 row-start-1 justify-self-end px-3 h-6 dark:text-red-400 text-red-500 pointer-events-none"
 				/>
 			)}
 		</span>
