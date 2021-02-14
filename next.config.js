df99e5dd87026634990fbbf095223aa72e2345d1
Kristóf Poduszló <kripod@protonmail.com>
2021-02-14 18:51:26 +0100

    feat(Input): invalid state

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules");
 
 module.exports = withPlugins(
-	[withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })],
+	[
+		withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" }),
+		withTM(["heroicons/react/outline", "heroicons/react/solid"]),
+	],
 	{
 		reactStrictMode: true,
 	},
--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,20 +1,28 @@
 import clsx from "clsx";
+import { ExclamationCircle } from "heroicons/react/outline";
 
 export type InputProps = React.ComponentPropsWithoutRef<"input"> & {
 	invalid?: boolean;
 };
 
-export function Input({ invalid, className, ...restProps }: InputProps) {
+export function Input({ invalid, className, style, ...restProps }: InputProps) {
 	return (
-		<input
-			className={clsx(
-				"placeholder-gray-700 dark:placeholder-gray-300 placeholder-opacity-75 dark:placeholder-opacity-60 px-3 h-11 bg-transparent border rounded focus:outline-none appearance-none dark:ring-offset-black ring-offset-1 ring-opacity-50 focus:ring",
-				invalid
-					? "border-red-500 ring-red-500"
-					: "border-gray-500 focus:border-blue-500 ring-blue-500",
-				className,
+		<span className={clsx("inline-grid items-center", className)} style={style}>
+			<input
+				className={clsx(
+					"placeholder-gray-700 dark:placeholder-gray-300 placeholder-opacity-75 dark:placeholder-opacity-60 col-start-1 row-start-1 px-3 h-11 bg-transparent rounded focus:outline-none appearance-none dark:ring-offset-black ring-offset-1 ring-opacity-50 focus:ring",
+					invalid
+						? "pr-11 border-2 border-red-500 ring-red-500"
+						: "border border-gray-500 focus:border-blue-500 ring-blue-500",
+				)}
+				{...restProps}
+			/>
+			{invalid && (
+				<ExclamationCircle
+					aria-hidden
+					className="col-start-1 row-start-1 justify-self-end px-3 h-6 text-red-500 pointer-events-none"
+				/>
 			)}
-			{...restProps}
-		/>
+		</span>
 	);
 }
