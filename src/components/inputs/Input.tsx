5f3b31f11e6005abed619222bfe9c2b4a8410f7e
Kristóf Poduszló <kripod@protonmail.com>
2021-02-27 23:52:45 +0100

    fix(Input): light color shades

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -12,15 +12,15 @@ export function Input({ invalid, className, ...restProps }: InputProps) {
 				className={clsx(
 					"placeholder-gray-600 dark:placeholder-gray-400 placeholder-opacity-80 dark:placeholder-opacity-80 col-start-1 row-start-1 px-3 h-11 bg-transparent rounded focus:outline-none appearance-none dark:ring-offset-black ring-offset-1 ring-opacity-50 dark:ring-opacity-50 focus:ring",
 					invalid
-						? "pr-11 border-2 border-red-600 dark:border-red-400 ring-red-600 dark:ring-red-400"
-						: "border border-gray-500 focus:border-blue-600 dark:focus:border-blue-400 ring-blue-600 dark:ring-blue-400",
+						? "pr-11 border-2 border-red-500 dark:border-red-400 ring-red-500 dark:ring-red-400"
+						: "border border-gray-500 focus:border-blue-500 dark:focus:border-blue-400 ring-blue-500 dark:ring-blue-400",
 				)}
 				{...restProps}
 			/>
 			{invalid && (
 				<ExclamationCircle
 					aria-hidden
-					className="col-start-1 row-start-1 justify-self-end px-3 h-6 dark:text-red-400 text-red-600 pointer-events-none"
+					className="col-start-1 row-start-1 justify-self-end px-3 h-6 dark:text-red-400 text-red-500 pointer-events-none"
 				/>
 			)}
 		</span>
