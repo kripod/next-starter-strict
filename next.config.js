795ae9ec1aefa7fb492415b99d3f9093bfccb9bc
Kristóf Poduszló <kripod@protonmail.com>
2021-08-24 00:17:22 +0200

    feat(next-config): import types for autocompletion

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 const withPlugins = require("next-compose-plugins");
 
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+	experimental: {
+		esmExternals: true,
+	},
+	future: {
+		strictPostcssConfiguration: true,
+	},
+	reactStrictMode: true,
+};
+
 module.exports = withPlugins(
 	[withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })],
-	{
-		experimental: {
-			esmExternals: true,
-		},
-		future: {
-			strictPostcssConfiguration: true,
-		},
-		reactStrictMode: true,
-	},
+	nextConfig,
 );
