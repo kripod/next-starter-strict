d0b2b10e0c1f7a78b63115b31dfe4b8c601fddc6
Kristóf Poduszló <kripod@protonmail.com>
2022-01-11 17:54:05 +0100

    feat: js/jsx compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{js,ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
   darkMode: "class",
   theme: {
     extend: {
