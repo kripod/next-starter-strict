bab064345252b6cf6f1eb44850292137c64f3498
Kristóf Poduszló <kripod@protonmail.com>
2022-09-11 10:27:56 +0200

    refactor(Button): remove gap when equilateral

--- src/components/buttons/_Button.tsx
+++ src/components/buttons/_Button.tsx
@@ -22,12 +22,12 @@ export const Button = React.forwardRef(function Button(
         "inline-flex items-center justify-center font-medium ring-offset-2 transition focus:outline-none focus-visible:ring dark:ring-offset-black",
         {
           [clsx(
-            "h-8 gap-x-1.5 rounded-md text-sm leading-4",
-            equilateral ? "w-8" : "px-3",
+            "h-8 rounded-md text-sm leading-4",
+            equilateral ? "w-8" : "gap-x-1.5 px-3",
           )]: size === "sm",
           [clsx(
-            "h-12 gap-x-2 rounded-lg text-base leading-5",
-            equilateral ? "w-12" : "px-5",
+            "h-12 rounded-lg text-base leading-5",
+            equilateral ? "w-12" : "gap-x-2 px-5",
           )]: size === "md",
         },
         disabled && "cursor-not-allowed",
