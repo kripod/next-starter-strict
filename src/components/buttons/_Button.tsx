8c43b59ed1817d98c4292bceb3dc7d969a9dc636
Kristóf Poduszló <kripod@protonmail.com>
2022-10-03 23:50:34 +0200

    fix(Button): try fixing spinner positioning again

--- src/components/buttons/_Button.tsx
+++ src/components/buttons/_Button.tsx
@@ -61,7 +61,7 @@ export const Button = React.forwardRef(function Button(
       </span>
 
       {loading ? (
-        <span className="col-start-1 row-start-1 inline-flex">
+        <span className="col-start-1 row-start-1">
           <Spinner size={size} />
         </span>
       ) : null}
