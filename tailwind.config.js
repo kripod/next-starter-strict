5119b4b4f84c31e8271240bf93a232bfa571a9ed
Kristóf Poduszló <kripod@protonmail.com>
2022-10-03 20:46:46 +0200

    feat(tailwind-config): enable a future option

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   darkMode: "class",
   theme: {
     extend: {
